dcd57dacdb79c2aca1c56e28b810a7360493d35a
cheesetan <93832702+cheesetan@users.noreply.github.com>
2023-02-27 16:11:39 +0800

    Initial Commit

---
+++ MathX-macOS/MathX_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathX_macOSApp.swift
+//  MathX-macOS
+//
+//  Created by Tristan on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MathX_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
