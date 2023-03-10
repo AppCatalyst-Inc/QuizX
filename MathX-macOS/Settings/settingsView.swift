2e3cd985f264c165aa132f11f40051a3bba5493d
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2023-03-10 21:57:18 +0800

    Settings View

--- MathX-macOS/Settings/settingsView.swift
+++ MathX-macOS/Settings/settingsView.swift
@@ -1,32 +1,74 @@
 import SwiftUI
 
-struct settingsView: View {
+struct SettingsView: View {
+    
+    @State private var notificationsEnabled = UserDefaults.standard.bool(forKey: "notificationsEnabled")
+    @State private var email = UserDefaults.standard.string(forKey: "email") ?? ""
+    
     var body: some View {
-        VStack(spacing: 15) {
-            HStack {
-                VStack(alignment: .leading, spacing: 2) {
-                    HStack {
-                        Text("Settings") // title
-                            .font(.system(size: 30))
-                            .fontWeight(.bold)
-                            .padding(.vertical, 30)
-                            .padding(.leading, 10)
-                        
-                        Spacer()
-                    }
-                  
-                    // settings view here
-                    
+        VStack(alignment: .leading, spacing: 20) {
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Notification Settings")
+                    .font(.headline)
+                Toggle(isOn: $notificationsEnabled) {
+                    Text("Enable Notifications")
                 }
             }
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Account Settings")
+                    .font(.headline)
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Appearance")
+                    .font(.headline)
+                Picker(selection: .constant(0), label: Text("Theme")) {
+                    Text("Light").tag(0)
+                    Text("Dark").tag(1)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                
+                Picker(selection: .constant(1), label: Text("Font Size")) {
+                    Text("Small").tag(0)
+                    Text("Medium").tag(1)
+                    Text("Large").tag(2)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Button(action: {
+                saveSettings()
+            }) {
+                Text("Save Changes")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 30)
+                    .padding(.vertical, 10)
+                    .background(Color.red)
+                    .cornerRadius(10)
+            }
+            .buttonStyle(PlainButtonStyle())
+            
+            Spacer()
         }
-        .padding(.horizontal, 30)
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .padding()
+        .frame(minWidth: 400, minHeight: 400)
+        .onAppear {
+            NSApp.windows.first?.backgroundColor = NSColor.controlBackgroundColor
+        }
+    }
+    
+    private func saveSettings() {
+        UserDefaults.standard.set(notificationsEnabled, forKey: "notificationsEnabled")
+        UserDefaults.standard.set(email, forKey: "email")
     }
 }
 
-struct settingsView_Previews: PreviewProvider {
+struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        settingsView()
+        SettingsView()
     }
 }
